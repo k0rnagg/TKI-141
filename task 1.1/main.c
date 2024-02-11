f241e902dce6f5ffee80029a8bf102fa184f7c27
Gamer <sergeykornaukhov@gmail.com>
2024-02-11 13:26:49 +0300

    Закончена работа над 1.1

--- task 1.1/main.c
+++ task 1.1/main.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
+#include <math.h>
+
+/**
+ * @brief Функция расчета уравнения А
+ * @param x - первый аргумент функции
+ * @param y - второй аргумент функции
+ * @param z - третий аргумент функции
+ */
+    double def_A(double x, double y, double z);
+
+/**
+ * @brief Точка входа в программу
+ * @return 0 в случае успеха
+ */
+
+double def_B(double x, double y, double z);
+/**
+ * @brief Точка входа в программу
+ * @return 0 в случае успеха
+ */
 int main()
 {
-	printf("Hello world!");
-	
+    double x = 0.2;
+    double y = 0.004;
+    double z = 1.1;
+    double a = def_A(x, y, z);
+    double b = def_B(x, y, z);
+    printf("x = %lf y = %lf z = %lf\n", x, y, z);
+    printf("a = %lf b = %lf", a, b);
+
+    return 0;
+}
+
+double def_A(double x, double y, double z)
+{
+    double a = pow(sin(pow(x, 2) + z), 2) - sqrt(x / y);
+    return a;
+}
+double def_B(double x, double y, double z)
+{
+    double b = pow(x, 2) / z + cos(pow((x + y), 3));
+    return b;
 }
